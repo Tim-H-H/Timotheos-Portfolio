5af50cd33bd8dc9917931b9750fcadba875047d6
Tim-H-H <timh.hector@hotmail.com>
2025-10-02 17:08:54 +0200

    created a simple contact page with all info and little animation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,19 @@
 export default function Contact() {
     return (
-        <section className="contact">
-            <h2>Kontakt</h2>
+        <section className="contact-container">
+            <h2 className="contact-header">Kontakt</h2>
             <p>
                 Jag heter <strong>Timotheo</strong> och jobbar som frontend-utvecklare. Jag älskar att skapa enkla, tillgängliga och eleganta användarupplevelser
             </p>
             <div className="contact-info">
-                <p><strong>Email:</strong>timotheohh@outlook.com</p>
-                <p><strong>LinkedIn:</strong><link rel="url" href="https://www.linkedin.com/in/timotheo-hector-08283a22a/"/></p>
-                <p><strong>Github:</strong><link rel="url" href="https://github.com/Tim-H-H" />Here</p>
+                <p><strong>Email:</strong> timotheohh@outlook.com</p>
+
+                <p><strong>LinkedIn:</strong><a href="https://www.linkedin.com/in/timotheo-hector-08283a22a/" target="_blank" rel="noopener noreferrer">Here</a></p>
+
+                <p><strong>Github:</strong><a href="https://github.com/Tim-H-H" target="_blank" rel="noopener noreferrer">Here</a></p>
             </div>
+            
         </section>
     )
 }
+
