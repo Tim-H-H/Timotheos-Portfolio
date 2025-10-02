e420e69db22b684384dcdea080a3217e2c138f38
Tim-H-H <timh.hector@hotmail.com>
2025-10-02 11:40:46 +0200

    Made Timotheo a path to homepage, added fonts for webpage, added CSS for both homepage and header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,9 @@ export default function Home() {
     return (
         <section className="home">
             <div className="hero">
-                <h1>Välkommen! Jag skapar moderna och intuitiva webblösningar.</h1>
+                <h1>Välkommen!<br /><br />Jag skapar moderna och intuitiva webblösningar.</h1>
                 <br />
+                
                 <p>Jag bygger moderna och användarvänliga webbgränssnitt med React och CSS.</p>
                 <div className="hero-actions">
                     <button onClick={() => nav("/Projects")}>Se mina projekt</button>
