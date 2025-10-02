71092a02ab2c28a79b13a84885e6bf14bd348142
Tim-H-H <timh.hector@hotmail.com>
2025-10-02 21:47:20 +0200

    Created project page and projectcard, will now add css styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 // import Footer from "./components/Footer";
 import Home from "./pages/Home";
-// import Projects from "./pages/Projects";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
                 <div className="main-content">
                 <Routes>
                     <Route path="/" element={<Home/>} />
-                    {/* <Route path="/projects" element={<Projects/>} /> */}
+                    <Route path="/projects" element={<Projects/>} />
                     <Route path="/contact" element={<Contact/>} />
                 </Routes>
                 </div>
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,56 @@
+import React, {useEffect, useState } from "react";
+import Projectcard from "../components/ProjectCard";
+
+export default function Projects() {
+    const curated = [
+        {
+            title: "Weather App",
+            description: "Weather forecast app using Openweather API",
+            github: "https://github.com/Tim-H-H/weather-app"
+        },
+    ];
+
+
+const [repos, setRepos] = useState([]);
+
+useEffect(() => {
+    fetch("https://api.github.com/users/Tim-H-H/repos")
+    .then((res) => res.json())
+    .then((data) => {
+        const clean = data.filter(
+            (repo) => !repo.fork && repo.description
+        );
+        setRepos(clean);
+    });
+}, []);
+
+return (
+    <section className="projects-container">
+        <h2 className="projects-header">Featured Projects</h2>
+        <div className="project-grid">
+            {curated.map((project, index) => (
+                <Projectcard
+                    key={index}
+                    title={project.title}
+                    description={project.description}
+                    github={project.github}
+                />
+            ))}
+        </div>
+
+        <h2 className="projects-header" style={{ marginTop: "3rem"}}>
+            Other Github Repos
+        </h2>
+        <div className="projects-grid">
+            {repos.slice(0, 6).map((repo) => (
+                <Projectcard
+                    key={repo.id}
+                    title={repo.name}
+                    description={repo.description}
+                    github={repo.html_url}
+                />
+            ))}
+        </div>
+    </section>
+)
+}
