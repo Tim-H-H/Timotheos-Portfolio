8f9aadf41fbfaa88d17754c2d63d62c5deb4acaf
Tim-H-H <timh.hector@hotmail.com>
2025-10-02 15:22:34 +0200

    beginning small contact, forgot to switch to the correct branch before starting. Small mistake

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 // import Footer from "./components/Footer";
 import Home from "./pages/Home";
 // import Projects from "./pages/Projects";
-// import Contact from "./pages/Contact";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
@@ -15,7 +15,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     {/* <Route path="/projects" element={<Projects/>} /> */}
-                    {/* <Route path="/contact" element={<Contact/>} /> */}
+                    <Route path="/contact" element={<Contact/>} />
                 </Routes>
                 </div>
             </main>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+export default function Contact() {
+    return (
+        <section className="contact">
+            <h2>Kontakt</h2>
+            <p>
+                Jag heter <strong>Timotheo</strong> och jobbar som frontend-utvecklare. Jag älskar att skapa enkla, tillgängliga och eleganta användarupplevelser
+            </p>
+            <div className="contact-info">
+                <p><strong>Email:</strong>timotheohh@outlook.com</p>
+                <p><strong>LinkedIn:</strong><link rel="url" href="https://www.linkedin.com/in/timotheo-hector-08283a22a/"/></p>
+                <p><strong>Github:</strong><link rel="url" href="https://github.com/Tim-H-H" />Here</p>
+            </div>
+        </section>
+    )
+}
