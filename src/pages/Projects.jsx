a7867d6d52b2d6dd84c03ab12067c4276dbc4760
Tim-H-H <timh.hector@hotmail.com>
2025-10-03 14:32:25 +0200

    got rid of featured projects and only show my whole projects as a whole.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,55 +2,49 @@ import React, {useEffect, useState } from "react";
 import Projectcard from "../components/ProjectCard";
 
 export default function Projects() {
-    const curated = [
-        {
-            title: "Weather App",
-            description: "Weather forecast app using Openweather API",
-            github: "https://github.com/Tim-H-H/weather-app"
-        },
-    ];
 
+    const [repos, setRepos] = useState([]);
 
-const [repos, setRepos] = useState([]);
+// TODO: Change clean to a better variable name
 
-useEffect(() => {
-    fetch("https://api.github.com/users/Tim-H-H/repos")
-    .then((res) => res.json())
-    .then((data) => {
-        const clean = data.filter(
-            (repo) => !repo.fork && repo.description
-        );
-        setRepos(clean);
-    });
-}, []);
+    useEffect(() => {
+        fetch("https://api.github.com/users/Tim-H-H/repos")
+        .then((res) => res.json())
+        .then((data) => {
+            const result = data.filter(
+                (repo) => !repo.fork && repo.description
+            );
+            setRepos(result);
+        });
+    }, []);
 
-return (
-    <section className="projects-container">
-        <h2 className="projects-header">Featured Projects</h2>
-        <div className="project-grid">
-            {curated.map((project, index) => (
-                <Projectcard
-                    key={index}
-                    title={project.title}
-                    description={project.description}
-                    github={project.github}
-                />
-            ))}
-        </div>
+    return (
+        <section className="projects-container">
+            {/* <h2 className="projects-header"></h2> */}
+            {/* <div className="project-grid">
+                {curated.map((project, index) => (
+                    <Projectcard
+                        key={index}
+                        title={project.title}
+                        description={project.description}
+                        github={project.github}
+                    />
+                ))}
+            </div> */}
 
-        <h2 className="projects-header" style={{ marginTop: "3rem"}}>
-            Other Github Repos
-        </h2>
-        <div className="projects-grid">
-            {repos.slice(0, 8).map((repo) => (
-                <Projectcard
-                    key={repo.id}
-                    title={repo.name}
-                    description={repo.description}
-                    github={repo.html_url}
-                />
-            ))}
-        </div>
-    </section>
-)
+            <h2 className="projects-header" style={{ marginTop: "3rem"}}>
+                Projects
+            </h2>
+            <div className="projects-grid">
+                {repos.slice(0, 8).map((repo) => (
+                    <Projectcard
+                        key={repo.id}
+                        title={repo.name}
+                        description={repo.description}
+                        github={repo.html_url}
+                    />
+                ))}
+            </div>
+        </section>
+    )
 }
